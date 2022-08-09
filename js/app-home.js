8559848b0efba989f43fe01118e1c6abb6451576
Danyer Jimenez <danyerjc@gmail.com>
2022-08-09 10:34:58 -0400

    Initial commit

---
+++ js/app-home.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    // $("a img, .btn-lots").on("click", function(e) {
+    //     console.log("Click");
+    //     // Globals
+    //     e.stopImmediatePropagation();
+    //     let lots = Number($(this).attr('data-lot'));
+    //     let src = $(this).attr('src');
+    //     console.log(src);
+    //     $('.num-lot').text(lots);
+    //     $('#img-lot').attr("src", src);
+    
+    //     console.log("lote No: " + lots);
+    //     $("#modal-lots").modal("show");
+    
+    // });
+    
+    $(".carousel-inner img, .btn-investments").on("click", function(e) {
+        // Globals
+        e.stopImmediatePropagation();
+        let url_page = '';
+        let url_card = $(this).attr('data-href');
+        let url_img = $(this).closest('.carousel').attr('data-href');
+        if (url_card !== undefined) {
+            url_page = url_card;
+        } else {
+            url_page = url_img;
+        }
+        window.open(url_page, '_blank');
+    
+    });
+});
